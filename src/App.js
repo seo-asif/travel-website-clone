c41449596fee90aa106539c20a7ca583ae8038e2
Asif Mosharraf <asif064@gmail.com>
2023-06-05 00:24:28 +0600

    Components Add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import About from './Components/About/About'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
-        
+        <About></About>
     </div>
   );
 }

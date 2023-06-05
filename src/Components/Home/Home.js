c8da79f0c3fa7be7b620ca26e9a2d43692a6be42
Asif Mosharraf <asif064@gmail.com>
2023-06-05 10:24:42 +0600

    Responsive Banner Complete

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,7 +2,42 @@ import React from "react";
 import "./home.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <section className="home">
+      <div className="secContainer container">
+        <div className="homeText">
+          <h1 className="title">Plan Your Trip With Travel Dot</h1>
+
+          <p className="subtitle">
+            Travel to your favourite city with respectful of the envireonment!
+          </p>
+
+          <button className="btn">
+            <a href="#">Explore Now</a>
+          </button>
+        </div>
+
+        <div className="homeCard grid">
+          <div className="locationDiv">
+            <label htmlFor="location">Location</label>
+            <input type="text" placeholder="Dream Destination" />
+          </div>
+
+          <div className="distDiv">
+            <label htmlFor="distance">Location</label>
+            <input type="text" placeholder="11/Meters" />
+          </div>
+
+          <div className="priceDiv">
+            <label htmlFor="price">Location</label>
+            <input type="text" placeholder="Dream $140-$500" />
+          </div>
+
+          <button className="btn ">Search</button>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Home;
